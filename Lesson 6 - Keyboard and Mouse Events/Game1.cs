b85a3e7228587012a6ba3b53915e416ce1700de1
Carsen Greenwood <362570749@GoTVDSB.ca>
2024-12-11 09:31:29 -0500

    Finished

--- Lesson 6 - Keyboard and Mouse Events/Game1.cs
+++ Lesson 6 - Keyboard and Mouse Events/Game1.cs
@@ -9,11 +9,21 @@ namespace Lesson_6___Keyboard_and_Mouse_Events
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        KeyboardState keyboardState;
+
+        Texture2D pacUpTexture;
+        Texture2D pacDownTexture;
+        Texture2D pacLeftTexture;
+        Texture2D pacRightTexture;
+        Texture2D pacSleepTexture;
         Texture2D pacTexture;
+
+        Rectangle window;
+
         Rectangle pacLocation;
         Vector2 pacSpeed;
 
-        KeyboardState keyboardState;
+        MouseState mouseState;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -23,8 +33,10 @@ namespace Lesson_6___Keyboard_and_Mouse_Events
 
         protected override void Initialize()
         {
+            window = new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight);
             // TODO: Add your initialization logic here
             pacLocation = new Rectangle(10, 10, 75, 75);
+            pacSpeed = new Vector2();
             base.Initialize();
         }
 
@@ -33,33 +45,58 @@ namespace Lesson_6___Keyboard_and_Mouse_Events
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
-            pacTexture = Content.Load<Texture2D>("PacRight");
+            pacDownTexture = Content.Load<Texture2D>("PacDown");
+            pacUpTexture = Content.Load<Texture2D>("PacUp");
+            pacLeftTexture = Content.Load<Texture2D>("PacLeft");
+            pacRightTexture = Content.Load<Texture2D>("PacRight");
+            pacSleepTexture = Content.Load<Texture2D>("PacSleep");
+            pacTexture = pacSleepTexture;
         }
 
         protected override void Update(GameTime gameTime)
         {
+            keyboardState = Keyboard.GetState();
+            pacSpeed.X = 0;
+            pacSpeed.Y = 0;
+            mouseState = Mouse.GetState();
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
-            pacSpeed = new Vector2();
-            keyboardState = Keyboard.GetState();
-            pacSpeed = Vector2.Zero;
             if (keyboardState.IsKeyDown(Keys.Up))
             {
+                pacTexture = pacUpTexture;
                 pacSpeed.Y -= 2;
             }
             if (keyboardState.IsKeyDown(Keys.Down))
             {
+                pacTexture = pacDownTexture;
                 pacSpeed.Y += 2;
             }
             if (keyboardState.IsKeyDown(Keys.Left))
             {
+                pacTexture = pacLeftTexture;
                 pacSpeed.X -= 2;
             }
             if (keyboardState.IsKeyDown(Keys.Right))
             {
+                pacTexture = pacRightTexture;
                 pacSpeed.X += 2;
+            }
+
+            pacLocation.X += (int)pacSpeed.X;
+            pacLocation.Y += (int)pacSpeed.Y;
+
+            if (!window.Contains(pacLocation))
+            {
+                pacLocation.X -= (int)pacSpeed.X;
+                pacLocation.Y -= (int)pacSpeed.Y;
+            }
+
+            if (!keyboardState.IsKeyDown(Keys.Up) && !keyboardState.IsKeyDown(Keys.Right) && !keyboardState.IsKeyDown(Keys.Left) && !keyboardState.IsKeyDown(Keys.Down))
+            {
+                pacTexture = pacSleepTexture;
             }
             base.Update(gameTime);
         }
@@ -69,6 +106,13 @@ namespace Lesson_6___Keyboard_and_Mouse_Events
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+
+            _spriteBatch.Draw(pacTexture, pacLocation, Color.White);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
 
             base.Draw(gameTime);
         }
